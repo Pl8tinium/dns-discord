68a67d9b2fdf488e1b252edafc56277c2f4a9363
Pl8tinium <pott-alexander@web.de>
2022-04-07 16:12:59 +0200

    init

---
+++ Readme.md
@@ -0,0 +1,6 @@
+## How to install
+- follow the [tutorial](https://www.freecodecamp.org/news/create-a-discord-bot-with-javascript-nodejs/) to create the bot account
+- uncheck "public bot" at the bot creation step 
+- for now the bots permissions can be restricted to only "Send Messages", set this property at the "OAuth2 -> Url Generator" tab after selecting the scope "bot"
+- login with the received url and pick the server the bot should join to
+- paste the token received in the menu "Bot -> Token -> Reset Token" into the js script
---
+++ discordBot.js
@@ -0,0 +1,46 @@
+const { Client, Intents } = require("discord.js");
+const { EventEmitter } = require("stream");
+
+
+botToken = 'OTYxNjEwMTg4MjMyNzI4NTc2.Yk7fRg.wWq39sw0Nrpp8TylIX1hYty17O8'
+
+class DiscordBot {    
+    #client = null
+    #loggedIn = false
+    loginSuccessful = new EventEmitter()
+
+    constructor() {
+        this.client = new Client({ intents: [Intents.FLAGS.GUILDS] })
+
+        this.client.on("ready", () => {
+            console.log(`Logged in as ${this.client.user.tag}!`)
+            this.loggedIn = true
+            this.loginSuccessful.emit('');            
+        })
+
+        this.client.login(botToken)
+    }
+
+    sendNotificationToUsers(userIds, message) {
+        if (this.loggedIn) {
+            console.log(`Sending message to ${userIds.length} users`, message)
+            userIds.forEach(userId => {
+                this.client.users.fetch(userId).then(user => {
+                    user.send(message)
+                })
+            })
+        } else {
+            console.log('Not logged in, yet')
+        }        
+    }
+}
+
+// debug
+bot = new DiscordBot()
+testIds = ['368485242073513994', '368485242073513994']
+propagatedMessage = 'testmeee'
+bot.loginSuccessful.on('', () => {
+    bot.sendNotificationToUsers(testIds, propagatedMessage)
+});
+
+module.exports.DiscordBot = DiscordBot
